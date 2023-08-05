56f99ef9f669f817081a3ae62047f314b4839f25
VincentB00 <vincentbui1999@gmail.com>
2023-08-05 16:22:17 -0400

    add basic organization creation input

--- src/app/organization/organization-creation/organization-creation.component.ts
+++ src/app/organization/organization-creation/organization-creation.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FixChangeDetection } from 'src/app/shared/directive/FixChangeDetection';
+import { Organization, OrganizationProfile, SMTP } from 'src/app/shared/model/Organization.model';
 
 @Component({
   selector: 'app-organization-creation',
   templateUrl: './organization-creation.component.html',
   styleUrls: ['./organization-creation.component.scss']
 })
-export class OrganizationCreationComponent implements OnInit {
+export class OrganizationCreationComponent extends FixChangeDetection implements OnInit {
 
-  constructor() { }
+  organizationProfile: OrganizationProfile = {};
+  smtp: SMTP = {};
+  
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
